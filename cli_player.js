45f8f2b36d8febf9585487c84563b2b5cc020e95
yatinsingh2007 <yatin4591@gmail.com>
2026-08-31 10:50:04 +0530

    feat: implement real-time playback progress bar and interval-based status tracking for CLI music player

--- cli_player.js
+++ cli_player.js
@@ -1,127 +1,142 @@
-const { readdirSync } = require("fs");
-const { spawn } = require("child_process");
-const { join } = require("path");
+const fs = require('fs');
+const path = require('path');
+const { spawn } = require('child_process');
 
+const songDir = path.join(__dirname, 'songs');
 let allSongs = null;
-let userSelectionIndex = 0;
-let currentSongProcess = null;
-let currentlyPlayingIndex = -1;
-let isPaused = false;
+let cursor = 0;
+let isPaused = true;
+let vlcPlayProcess = undefined;
 
-function listSongs(songDirPath) {
-    allSongs = readdirSync(songDirPath).filter(file => !file.startsWith('.'));
+let totalDuration = undefined;
+let timeElapsed = undefined;
 
-    console.clear();
-    console.log("=== Music Player ===");
-    console.log("Controls: [↑/↓] Navigate | [Enter] Play | [Space / P] Pause/Play | [Ctrl+C] Exit\n");
+async function getSongDuration(songFilePath) {
+    return new Promise((resolve, reject) => {
+        const afinfoCP = spawn("afinfo", [songFilePath])
 
-    allSongs.forEach((data, index) => {
-        const isSelected = userSelectionIndex === index;
-        const isPlayingThis = currentlyPlayingIndex === index;
-
-        let statusSymbol = " ";
-        if (isPlayingThis) {
-            statusSymbol = isPaused ? "⏸" : "▶";
-        }
-
-        const cursor = isSelected ? ">" : " ";
-        console.log(`${cursor} ${statusSymbol} ${data}`);
-    });
-
-    console.log("");
-    if (currentlyPlayingIndex !== -1 && currentSongProcess) {
-        const stateStr = isPaused ? "PAUSED" : "PLAYING";
-        console.log(`Status: [${stateStr}] ${allSongs[currentlyPlayingIndex]}`);
-    } else {
-        console.log("Status: [STOPPED]");
-    }
-}
-
-function stopCurrentSong() {
-    if (currentSongProcess) {
-        currentSongProcess.removeAllListeners('exit');
-        currentSongProcess.kill('SIGKILL');
-        currentSongProcess = null;
-    }
-}
-
-function playSong(songPath, songIndex) {
-    stopCurrentSong();
-
-    currentlyPlayingIndex = songIndex;
-    isPaused = false;
-
-    currentSongProcess = spawn('vlc', ['--intf', 'rc', songPath], {
-
-    });
-
-    currentSongProcess.on('error', (err) => {
-        console.error(`Error spawning afplay: ${err.message}`);
-    });
-
-    currentSongProcess.on('exit', () => {
-        currentSongProcess = null;
-        currentlyPlayingIndex = -1;
-        isPaused = false;
-        listSongs(join(__dirname, 'songs'));
-    });
-}
-
-function togglePausePlay() {
-    if (!currentSongProcess) {
-        if (allSongs && allSongs[userSelectionIndex]) {
-            playSong(join(__dirname, 'songs', allSongs[userSelectionIndex]), userSelectionIndex);
-        }
-        return;
-    }
-
-    if (isPaused) {
-        currentSongProcess.kill('SIGCONT');
-        isPaused = false;
-    } else {
-        currentSongProcess.kill('SIGSTOP');
-        isPaused = true;
-    }
-}
-
-
-function getSongDuration(songfilePath){
-    return new Promise((res , rej) => {
-        const afInfoCP = spawn('afinfo',[songfilePath]);
-        afInfoCP.stdout.on('data',data=>{
-            res(data.toString().split("estimated duration: ")[1].split(".")[0]);
+        afinfoCP.stdout.on('data', (data) => {
+            resolve(Number(data.toString().split("estimated duration: ")[1].split(".")[0]) + 1)
         })
     })
 }
-listSongs(join(__dirname, 'songs'));
-getSongDuration(join(__dirname, 'songs', allSongs[0])).then((duration) => {
-    console.log(duration);
-});
 
+function startElapsedTracking() {
+    timeElapsed = 0;
+    setInterval(() => {
+        if (vlcPlayProcess !== undefined && !isPaused) {
+            timeElapsed += 0.1
+        }
+
+        listSongs(songDir)
+    }, 100)
+}
+
+
+function renderBar(percentagePlayed) {
+    const PROGRESS_BAR_WIDTH = 50;
+
+    const playedCharC = Math.round(50 * (percentagePlayed) / 100)
+
+    const progressBar = "X".repeat(playedCharC) + ".".repeat(50 - playedCharC)
+    return progressBar
+}
+
+function listSongs(songDirPath) {
+    allSongs = fs.readdirSync(songDirPath);
+
+    process.stdout.write("\x1B[3;1H");
+
+    const menuText = allSongs.map((songName, index) => {
+        return ("\r\x1B[0K" + (index === cursor ? '>' : '') + songName)
+    }).join("\n")
+
+    process.stdout.write(menuText + "\n")
+
+
+    if (timeElapsed !== undefined && totalDuration !== undefined) {
+        const percentagePlayed = Math.min(100, ((timeElapsed / totalDuration) * 100).toFixed(2))
+        process.stdout.write(`\r\x1B[0K${Math.ceil(timeElapsed)} / ${totalDuration} || ${percentagePlayed} %`)
+        const bar = renderBar(percentagePlayed)
+        process.stdout.write(`\n\x1B[0K${bar}`)
+    }
+}
+
+
+async function playSong(cursor) {
+    if (vlcPlayProcess !== undefined) {
+        vlcPlayProcess.kill(15)
+        vlcPlayProcess = undefined
+    }
+
+    isPaused = false;
+    const songFinalPath = path.join(songDir, allSongs[cursor]);
+    totalDuration = await getSongDuration(songFinalPath)
+    startElapsedTracking()
+    vlcPlayProcess = spawn('vlc', ["--intf", "rc", songFinalPath]);
+}
+
+process.stdout.write('\x1b[2J');
+listSongs(songDir);
 
 process.stdin.setRawMode(true);
-process.stdin.resume();
-process.stdin.on('data', data => {
-    const final = data.toString();
+process.stdin.on('data', (data) => {
+    if (data[0] === 0x1b) {
+        if (data[1] === 0x5b) {
+            if (data[2] === 0x41) {
+                // up arrow key
+                cursor = ((cursor - 1) % allSongs.length); // should be in the loop of songs i.e use module operator
+                if (cursor < 0) {
+                    cursor += allSongs.length
+                }
+            } else if (data[2] === 0x42) {
+                cursor = (cursor + 1) % allSongs.length; // should be in the loop of songs i.e use module operator
+                // down arrow key
+            } else if (data[2] === 0x43) {
+                // right arrow key
 
-    // Exit on Ctrl+C
-    if (final === '\u0003') {
-        stopCurrentSong();
-        process.exit(0);
+            } else if (data[2] === 0x44) {
+
+                // left arrow key
+            }
+        }
+
+        listSongs(songDir)
+        return
     }
 
-    if (allSongs && allSongs.length > 0) {
-        if (final === '\u001b[A') { // Up arrow - cyclic navigation
-            userSelectionIndex = (userSelectionIndex - 1 + allSongs.length) % allSongs.length;
-        } else if (final === '\u001b[B') { // Down arrow - cyclic navigation
-            userSelectionIndex = (userSelectionIndex + 1) % allSongs.length;
-        } else if (final === '\r' || final === '\n') { // Enter key
-            playSong(join(__dirname, 'songs', allSongs[userSelectionIndex]), userSelectionIndex);
-        } else if (final === ' ' || final.toLowerCase() === 'p') { // Spacebar or P key
-            togglePausePlay();
+    // next and back in raw mode
+    if (data[0] === 110) { // Play Next
+        cursor = (cursor + 1) % allSongs.length; // should be in the loop of songs i.e use module operator
+        listSongs(songDir)
+        playSong(cursor)
+    }
+
+    if (data[0] === 98) { // Play Previous
+        cursor = ((cursor - 1) % allSongs.length); // should be in the loop of songs i.e use module operator
+        if (cursor < 0) {
+            cursor += allSongs.length
+        } listSongs(songDir)
+        playSong(cursor)
+    }
+
+    // enter in raw mode
+    if (data[0] === 0x0d) {
+        playSong(cursor);
+        return;
+    }
+
+    // when in raw mode, the process get 0x03, does not provide SIGINT signal ( this gets in default mode )
+    if (data[0] === 0x03) {
+        process.exit(); // generates SIGINT and exit the nodejs process
+    }
+
+    // play pause
+    if (data[0] === 112) {
+        isPaused = !isPaused
+        // to stop the process we will use SIGSTOP command
+        if (vlcPlayProcess !== undefined) {
+            vlcPlayProcess.stdin.write('pause\n');
         }
     }
-
-    listSongs(join(__dirname, 'songs'));
-});
-
+})
